98ecf00f6a661d3dd2a18944064a5b1bd77442f2
John-Weetaker <jjdormevil@gmail.com>
2017-10-23 10:24:56 +0200

    lazy-once option

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ import * as helo from './log.js'
 
 
 document.getElementById('btn_lazy').addEventListener('click', function(){
-    import(/* webpackMode: "lazy" */'jquery').then(($) => {
+    import(/* webpackMode: "lazy-once" */'jquery').then(($) => {
         helo.helo2()
         let chocolat = 'nestlet'
         let [a, ,b] = [1,2,3,4]
